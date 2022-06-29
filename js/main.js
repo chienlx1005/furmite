1f2ade0a442188c6c64b7f466f8af8d0e18c4584
chienlx1005 <chienlx1005@gmail.com>
2022-06-29 14:36:39 +0700

    fix modal

--- js/main.js
+++ js/main.js
@@ -4,41 +4,25 @@ const $$ = document.querySelectorAll.bind(document);
 const menu = $(".icon-menu");
 const closeBtn = $(".icon-close");
 const modal = $(".modal-content");
-const footerMenu = $(".head-control-footer");
-const footerCopyRight = $(".footer-copyright");
+
 const header = $(".header");
 
 const app = {
   showModal: function () {
     menu.onclick = () => {
       modal.classList.add("modal-content-active");
-      menu.style.display = "none";
+      // menu.style.display = "none";
     };
     closeBtn.onclick = () => {
       modal.classList.remove("modal-content-active");
-      menu.style.display = "flex";
+      // menu.style.display = "flex";
     };
-  },
-  modalHide: function () {
-    window.addEventListener("resize", function () {
-      if (window.innerWidth >= 1000) {
-        const modalOpen = $(".modal-content.modal-content-active");
-        if (modalOpen) {
-          modalOpen.classList.toggle("modal-content-active");
-          menu.style.display = "flex";
-        }
-      }
-      this.window.innerWidth <= 990
-        ? ((footerMenu.style.display = "flex"),
-          footerCopyRight.classList.add("footer-copyright-active"))
-        : ((footerMenu.style.display = "none"),
-          footerCopyRight.classList.remove("footer-copyright-active"));
-    });
   },
 
   start: function () {
     this.showModal();
     this.modalHide();
+    this.showMenu();
   },
 };
 app.start();
